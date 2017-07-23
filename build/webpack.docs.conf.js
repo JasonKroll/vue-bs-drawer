dd2482d92d7a22df75027815db67b31ad29da131
Jason Kroll <krolly@gmail.com>
2017-07-23 16:51:29 +1000

    Fixed path error

--- build/webpack.docs.conf.js
+++ build/webpack.docs.conf.js
@@ -39,7 +39,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('../docs/css/[name].[contenthash].css')
+      filename: utils.assetsPath('../css/[name].[contenthash].css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
